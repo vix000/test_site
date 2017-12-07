c66add647428ffd669da22d15d9f4bf79a190dc8
Your Name <you@example.com>
2017-12-07 17:18:23 +0100

    Editing git

--- login/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from login.models import Post, Friend
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Friend) #this materializes Friend model in admin panel
