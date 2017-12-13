c11379f56cc4feb9e01c4913ae25ee30734c47b8
Your Name <you@example.com>
2017-12-13 16:33:25 +0100

    Working with details

---
+++ nextCRM_1/blog/urls.py
@@ -0,0 +1,55 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from login import views as myapp_views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+app_name = 'post'
+
+urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', auth_views.login),
+    url(r'^home/$', myapp_views.home, name='home'),
+    url(r'^register/$', myapp_views.register, name='register'),
+
+    url(r'companies/(?P<pk>\d+)/comment/$', myapp_views.add_comment, name='add_comment'),
+    url(r'^register/success/$', myapp_views.register_success),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'companies/$', myapp_views.CompaniesView.as_view(), name="companies"),
+    url(r'users/$', myapp_views.view_profile, name="view_profile"),
+    url(r'edit_profile/$', myapp_views.edit_profile, name="edit_profile"),
+    url(r'^change-password/$', myapp_views.change_password, name="change_password"),
+    url(r'^companies/$', myapp_views.CompanyDeleteView.as_view(), name='companies'),
+    url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', myapp_views.change_friends, name='change_friends'),
+    url(r'users/(?P<pk>\d+)/author_confirm_delete/', myapp_views.DeleteUser.as_view(), name='delete_user'),
+    url(r'users/(?P<pk>\d+)/user_update/', myapp_views.UpdateUser.as_view(), name='update_user'),
+    url(r'companies/(?P<pk>\d+)/company_confirm_delete/', myapp_views.DeleteCompany.as_view(), name='delete_company'),
+    url(r'companies/(?P<pk>\d+)/company_update/', myapp_views.UpdateCompany.as_view(), name="edit_company"),
+    url(r'companies/(?P<pk>\d+)/company_details/', myapp_views.CompanyDetail.as_view(), name="company_details"),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_'),
+    url(r'meetings/add_meeting/$', myapp_views.MeetingAdd.as_view(), name='add_meeting'),
+    url(r'meetings/$', myapp_views.MeetingView.as_view(), name='meetings'),
+    url(r'meetings/(?P<pk>\d+)/my_meetings/$', myapp_views.MeetingView.as_view(), name='my_meetings'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ nextCRM_1/login/models.py
@@ -0,0 +1,76 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+# Create your models here.
+class Post(models.Model):
+	post = models.CharField(max_length=500)
+	location = models.CharField(max_length=500, default=None)
+	# phone_number = PhoneNumberField(default=None)
+	info = models.CharField(max_length=500, default='')
+	user = models.ForeignKey(User)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	categories = (
+		('Food', 'Food'),
+		('Transport', 'Transport'),
+		('IT', 'IT'),
+		('Logistics', 'Logistics'),
+		('Other', 'Other'),
+	)
+	category = models.CharField(max_length=9, choices=categories, blank=True)
+	image = models.ImageField(upload_to='profile_image', blank=True, null=True) #blank means it can be optional
+
+	def __str__(self):
+		return self.post
+
+
+class Friend(models.Model):
+	users = models.ManyToManyField(User) #all the other users
+	current_user = models.ForeignKey(User, related_name='owner', null=True) #user currently logged in
+
+	@classmethod
+	def make_friend(cls, current_user, new_friend):
+		friend, created = cls.objects.get_or_create(
+			current_user=current_user
+			)
+		friend.users.add(new_friend)
+
+	@classmethod
+	def lose_friend(cls, current_user, new_friend):
+		friend, created = cls.objects.get_or_create(
+			current_user=current_user
+			)
+		friend.users.remove(new_friend)
+
+
+
+class CompanyComment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    user = models.CharField(max_length=250)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default = False)
+
+    def approved(self):
+        self.approved = True
+        self.save()
+
+    def __str__(self):
+        return self.user
+
+
+
+class Meeting(models.Model):
+	title = models.CharField(max_length=100)
+	company_name = models.ForeignKey(Post)
+	meeting_date = models.DateTimeField()
+	details = models.TextField(max_length=250)
+	created = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(User, null=True, default=User)
+	def __str__(self):
+		return self.title
